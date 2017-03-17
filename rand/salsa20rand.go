37f1946abd02bd02f0f12e4914f16150f4247ddb
David Lazar <lazard@csail.mit.edu>
2017-03-17 10:44:55 -0400

    all: add licenses and copyright notices

--- rand/salsa20rand.go
+++ rand/salsa20rand.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Vuvuzela Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package rand implements a faster CSPRNG.
 package rand // import "vuvuzela.io/crypto/rand"
 
